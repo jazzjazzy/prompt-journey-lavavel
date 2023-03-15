0cd712f4648bcc97345aa5c4427b1d498d2ea811
jazzjazzy <jsjazzau@gmail.com>
2023-03-15 21:16:29 +1100

    added video to login and app

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,9 +4,24 @@
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
+
+        <div class="flex justify-center items-center pb-10">
+            <div>
+                <a href="https://www.youtube.com/watch?v=X4_leGsfPME?autoplay=1&vq=hd1080&controls=1" target="_blank"
+                   class="popup-youtube inline-flex items-center px-1 pt-1 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">
+                    <div class="pt-3 flex justify-center items-center">
+                        <div>Watch me</div>
+                        <div><i class="ml-3 fa-solid fa-play-circle text text-red-400 text-[35px]"></i></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+
         <div>
             <a href="{{ route('register') }}"
-               class="w-full inline-flex justify-center items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Register now</a>
+               class="w-full inline-flex justify-center items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Register
+                now</a>
         </div>
         <hr class="border border-slate-300 mt-6 mb-6">
         <!-- Email Address -->
