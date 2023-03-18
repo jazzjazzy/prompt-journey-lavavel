5c6cb448d7c2458543b226ee9b08a8346c497194
jazzjazzy <jsjazzau@gmail.com>
2023-03-18 14:37:44 +1100

    made both the main and text prompts expand vertically if text is too long

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,11 +3,21 @@
 @section('content')
 <div class="grid grid-cols-12 pt-1 m-auto px-12">
     <div class="col-span-10 border-black border-4">
+        {{-- history past --}}
         <div id="pre-prompt-2" class="text-xs align-middle text-gray-300 h-6 pl-4 w-3/4 truncate"></div>
         <div id="pre-prompt-1" class="text-sm align-middle text-gray-600 h-6 pl-4 w-3/4 truncate"></div>
+
+        {{-- *********************** --}}
+        {{-- This is the main prompt --}}
+        {{-- *********************** --}}
+        <div class="p-0 m-0 grow-wrap">
         <textarea
-            class="disabled: focus:outline-none w-full h-24 resize-none border border-gray-300 rounded-md px-4 py-2 bg-white col-span-full"
-            id="prompt"></textarea>
+            class="focus:outline-none w-full h-24 resize-none border border-gray-300 rounded-md px-4 py-2 bg-white col-span-full"
+            disabled
+            id="prompt">
+        </textarea>
+        </div>
+        {{-- history future --}}
         <div id="post-prompt-1" class="text-sm align-middle text-gray-600 h-6 pl-4 w-3/4 truncate"></div>
         <div id="post-prompt-2" class="text-xs align-middle text-gray-300 h-6 pl-4 w-3/4 truncate"></div>
     </div>
@@ -16,6 +26,7 @@
         <button id="copyMjButton" title="Ctrl + shift + c" class="btn btn-primary m-4 h-fit w-fit">
             <i class="text-[130px] p-4 fas fa-copy"></i>
         </button>
+        {{--  Main prompt copy button --}}
         <div class="alert alert-notice mx-3 hidden" id="copy-mj-prompt">
             suffix copied to clipboard
         </div>
@@ -35,9 +46,13 @@
                             <div class="input-prompt-fields">
                                 <div class="grid grid-cols-12 gap-2">
                                     <div class="col-span-12 grow-wrap">
+
+                                        {{-- ************************************** --}}
+                                        {{-- This is the input field for the prompt --}}
+                                        {{-- ************************************** --}}
                                         <textarea name="text" id="prompt-text"
-                                                  onInput="this.parentNode.dataset.replicatedValue = this.value"
-                                                  class="prompt-text-class mt-0" type="text"
+                                                  onInput="$.expandTextarea(this)"
+                                                  class="prompt-text-class mt-0 w-full overflow-auto" type="text"
                                                   title="ctrl-space"></textarea>
                                     </div>
                                 </div>
@@ -303,119 +318,7 @@
                 </div>
             </div>
         </div>
-
-        <div class="card">
-            <div class="card-header">
-                Shortcut List
-            </div>
-            <div class="card-body grid grid-cols-2 gap-x-3 gap-y-2">
-                <div class="col-span-full border-b border-gray-400">
-                    Main Shortcut
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Ctrl + Space</span>
-                    <span class="text-gray-900 font-medium text-xs">focus prompt</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Ctrl + shift + c:</span>
-                    <span class="text-gray-900 font-medium text-xs">copy Mj Prompt</span>
-                </div>
-                <div class="col-span-full border-b border-gray-400">
-                    Action Shortcut
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Ctrl + Alt + Shift + s:</span>
-                    <span class="text-gray-900 font-medium text-xs">add as Suffix</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Ctrl + Alt + Shift + F4:</span>
-                    <span class="text-gray-900 font-medium text-xs">Clear all</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Ctrl + Alt + Shift + h:</span>
-                    <span class="text-gray-900 font-medium text-xs">history</span>
-                </div>
-                <div class="col-span-full border-b border-gray-400">
-                    Basic Param shortcuts
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Ctrl + Alt + a:</span>
-                    <span class="text-gray-900 font-medium text-xs">aspect </span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Ctrl + Alt + c:</span>
-                    <span class="text-gray-900 font-medium text-xs">chaos</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Ctrl + Alt + q:</span>
-                    <span class="text-gray-900 font-medium text-xs">quality</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Ctrl + Alt + n:</span>
-                    <span class="text-gray-900 font-medium text-xs">no</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Ctrl + Alt + e:</span>
-                    <span class="text-gray-900 font-medium text-xs">seed</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Ctrl + Alt + t:</span>
-                    <span class="text-gray-900 font-medium text-xs">stop</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Ctrl + Alt + y:</span>
-                    <span class="text-gray-900 font-medium text-xs">style</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Ctrl + Alt + v:</span>
-                    <span class="text-gray-900 font-medium text-xs">version</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Ctrl + Alt + s:</span>
-                    <span class="text-gray-900 font-medium text-xs">stylize</span>
-                </div>
-                <div class="col-span-full border-b border-gray-400">
-                    Model version Param shortcuts
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Alt + n:</span>
-                    <span class="text-gray-900 font-medium text-xs">niji</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Alt + h:</span>
-                    <span class="text-gray-900 font-medium text-xs">hd</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Alt + t:</span>
-                    <span class="text-gray-900 font-medium text-xs">test</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Alt + p:</span>
-                    <span class="text-gray-900 font-medium text-xs">testp</span>
-                </div>
-                <div class="col-span-full border-b border-gray-400">
-                    Upscaler Param shortcuts
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Alt + l:</span>
-                    <span class="text-gray-900 font-medium text-xs">uplight</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4">Alt + b:</span>
-                    <span class="text-gray-900 font-medium text-xs">upbeta</span>
-                </div>
-                <div class="col-span-1 flex flex-row items-center justify-between">
-                    <span class="text-gray-500 font-medium text-xs mr-4"> Alt + a:</span>
-                    <span class="text-gray-900 font-medium text-xs">upanime</span>
-                </div>
-            </div>
-
-            <div class="card-footer">
-                <div class="text-[10px]">
-                    *NOTE: Depending on the browser or operating system you are using, some of these shortcuts might not function as expected.
-                </div>
-            </div>
-        </div>
+        @include('shortcuts')
     </div>
 </div>
 
@@ -427,6 +330,7 @@
 
 @section('scripts')
 <script type="module">
+
 
     $(document).ready(function () {
 
