c629f12209029577e166be1b3aade51f2bbd28f5
jazzjazzy <jsjazzau@gmail.com>
2023-03-20 14:39:47 +1100

    fixed issue with history up/down not working correctly

--- resources/js/pj-js/history.js
+++ resources/js/pj-js/history.js
@@ -1,6 +1,7 @@
-$(document).ready(function () {
+window.savedStrings = [];
+window.currentIndex = window.savedStrings.length;
 
-    var savedStrings = [];
+$(document).ready(function () {
 
     // Attach a 'click' event listener to the button
     $('#copyMjButton').on('click', function () {
@@ -14,39 +15,55 @@ $(document).ready(function () {
         // Get the value of the input field
         var inputVal = $('#prompt').val();
         // Copy the value to the clipboard
-        navigator.clipboard.writeText("/imagine prompt: "+inputVal);
+        navigator.clipboard.writeText("/imagine prompt: " + inputVal);
 
-        var nextIndex = savedStrings.length;
+        var nextIndex = window.savedStrings.length;
         // Add the value to the array of saved strings
-        savedStrings.push(inputVal);
-        $('#pre-prompt-1').text(savedStrings[nextIndex]);
-        $('#pre-prompt-2').text(savedStrings[nextIndex - 1]);
-        console.log(savedStrings);
+        window.savedStrings.push(inputVal);
+        $('#pre-prompt-1').text(window.savedStrings[nextIndex]);
+        $('#pre-prompt-2').text(window.savedStrings[nextIndex - 1]);
+        window.currentIndex = window.savedStrings.length
         promptCopyNoticeAlert('#copy-mj-prompt', 'Prompt copied to clipboard');
+        console.log(window.savedStrings);
     }
 
     // Attach a 'keydown' event listener to the input field
     $(document).on('keydown', function (e) {
         if (e.keyCode === 38 || e.keyCode === 40) { // up or down arrow key
             e.preventDefault(); // stop default scrolling behavior
-            // Get the index of the current value in the array of saved strings
-            var currentIndex = savedStrings.indexOf($('#prompt').val());
-            // Calculate the index of the next or previous value
-            var nextIndex = currentIndex + (event.keyCode === 38 ? -1 : 1);
-            // Check if the index is within the bounds of the array
-            $('#pre-prompt-2').text('');
-            $('#pre-prompt-1').text('');
-            $('#post-prompt-2').text('');
-            $('#post-prompt-1').text('');
-            if (nextIndex >= 0 && nextIndex < savedStrings.length) {
-                $('#pre-prompt-2').text(savedStrings[nextIndex - 2]);
-                $('#pre-prompt-1').text(savedStrings[nextIndex - 1]);
-                // Set the value of the input field to the next or previous value
-                $('#prompt').val(savedStrings[nextIndex]);
 
-                $('#post-prompt-1').text(savedStrings[nextIndex + 1]);
-                $('#post-prompt-2').text(savedStrings[nextIndex + 2]);
+            // Calculate the index of the next or previous value
+            var nextIndex = window.currentIndex + (event.keyCode === 38 ? -1 : 1);
+
+            // Check if the index is within the bounds of the array
+            if (nextIndex >= 0 && nextIndex < window.savedStrings.length) {
+                // Update the current index
+                window.currentIndex = nextIndex;
+
+                // Clear the value of the input field
+                $('#pre-prompt-2').text('');
+                $('#pre-prompt-1').text('');
+                $('#post-prompt-2').text('');
+                $('#post-prompt-1').text('');
+
+                // Set the value of the input field to the next or previous value
+                $('#pre-prompt-2').text(window.savedStrings[nextIndex - 2]);
+                $('#pre-prompt-1').text(window.savedStrings[nextIndex - 1]);
+                // Set the value of the input field to the next or previous value
+                $('#prompt').val(window.savedStrings[nextIndex]);
+
+                $('#post-prompt-1').text(window.savedStrings[nextIndex + 1]);
+                $('#post-prompt-2').text(window.savedStrings[nextIndex + 2]);
             }
+            else if(nextIndex == window.savedStrings.length)
+            {
+                // reset the current index
+                window.currentIndex = window.savedStrings.length;
+                // update the master prompt with the current prompt and params
+                updatePromptText();
+            }
+
+            console.log(window.currentIndex);
         }
     });
 
@@ -65,7 +82,7 @@ $(document).ready(function () {
         showHistory();
     });
 
-    function showHistory(){
+    function showHistory() {
         let history = '<div class="overlay w-full">\n' +
             '        <div class="card w-1/2 bg-gray-100 w-3/4 h-full">\n' +
             '            <div class="max-w-7xl mx-auto">\n' +
@@ -73,14 +90,14 @@ $(document).ready(function () {
             '                <div class="card-body flex flex-col h-full">\n' +
             '                    <ul class="list-none pl-8 divide-y divide-gray-300 overflow-auto">';
 
-        if (savedStrings.length === 0) {
+        if (window.savedStrings.length === 0) {
             history = history + '<li class="py-4 px-6 {{ $loop->last ? \'\' : \'border-b\' }}">\n' +
                 '                       <p class="text-gray-500 leading-tight">\n' + 'No history yet!' + '</p>\n' +
                 '               </li>'
         } else {
-            for (let i = 0; i < savedStrings.length; i++) {
+            for (let i = 0; i < window.savedStrings.length; i++) {
                 history = history + '<li class="mb-4">\n' +
-                    '                       <p class="text-gray-500 leading-tight">\n' + savedStrings[i] + '</p>\n' +
+                    '                       <p class="text-gray-500 leading-tight">\n' + window.savedStrings[i] + '</p>\n' +
                     '               </li>'
             }
         }
@@ -113,10 +130,10 @@ $(document).ready(function () {
     };
 
     $("#clear-history").click(function () {
-        savedStrings = [];
+        window.savedStrings = [];
     });
 
-    $.extend( window,{
+    $.extend(window, {
         showHistory: showHistory,
         copyMjPrompt: copyMjPrompt
     })
