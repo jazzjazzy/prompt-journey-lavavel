937e76b13fe278380c5ceef93e61a966b0c237c4
jazzjazzy <jsjazzau@gmail.com>
2023-03-22 10:14:24 +1100

    - fixed the modal for show history - add a copy from history button - go clear history working - add v5 --time --iw

--- resources/js/pj-js/history.js
+++ resources/js/pj-js/history.js
@@ -13,18 +13,28 @@ $(document).ready(function () {
      */
     function copyMjPrompt() {
         // Get the value of the input field
-        var inputVal = $('#prompt').val();
-        // Copy the value to the clipboard
-        navigator.clipboard.writeText("/imagine prompt: " + inputVal);
+        var inputVal = $.trim($('#prompt').val());
 
-        var nextIndex = window.savedStrings.length;
-        // Add the value to the array of saved strings
-        window.savedStrings.push(inputVal);
-        $('#pre-prompt-1').text(window.savedStrings[nextIndex]);
-        $('#pre-prompt-2').text(window.savedStrings[nextIndex - 1]);
-        window.currentIndex = window.savedStrings.length
-        promptCopyNoticeAlert('#copy-mj-prompt', 'Prompt copied to clipboard');
-        console.log(window.savedStrings);
+        if (inputVal !== '' && inputVal !== null && inputVal !== undefined) {
+            // Copy the value to the clipboard
+            navigator.clipboard.writeText("/imagine prompt: " + inputVal);
+
+            var nextIndex = window.savedStrings.length;
+
+            // Add the value to the array of saved strings
+            window.savedStrings.push(inputVal);
+
+            // add last 2 prompts to the pre prompt
+            $('#pre-prompt-1').text(window.savedStrings[nextIndex]);
+            $('#pre-prompt-2').text(window.savedStrings[nextIndex - 1]);
+
+            //change the currentIndex to the new prompt index
+            window.currentIndex = window.savedStrings.length
+
+            //show alert that the prompt was copied
+            promptCopyNoticeAlert('#copy-mj-prompt', 'Prompt copied to clipboard');
+        }
+        //console.log(window.savedStrings);
     }
 
     // Attach a 'keydown' event listener to the input field
@@ -54,16 +64,14 @@ $(document).ready(function () {
 
                 $('#post-prompt-1').text(window.savedStrings[nextIndex + 1]);
                 $('#post-prompt-2').text(window.savedStrings[nextIndex + 2]);
-            }
-            else if(nextIndex == window.savedStrings.length)
-            {
+            } else if (nextIndex == window.savedStrings.length) {
                 // reset the current index
                 window.currentIndex = window.savedStrings.length;
                 // update the master prompt with the current prompt and params
                 updatePromptText();
             }
 
-            console.log(window.currentIndex);
+            //console.log(window.currentIndex);
         }
     });
 
@@ -79,58 +87,80 @@ $(document).ready(function () {
     }
 
     $("#show-history").click(function () {
+
         showHistory();
+        let cardHeight = $('#overlayHistory .card').height();
+        let cardHeaderHeight = $('#overlayHistory .card-header').height();
+        let cardFooterHeight = $('#overlayHistory .card-footer').height();
+        let viwportHeight = $(window).height();
+
+        //
+        if (cardHeight > viwportHeight) {
+            cardHeight = (viwportHeight - 100);
+            let container = (cardHeight - (cardHeaderHeight + cardFooterHeight));
+            container = container - Math.floor(container * 0.1); //remove offset 10%
+            $('#overlayHistory .card-body').attr('style', 'height: ' + container + 'px');
+        }
+
     });
 
     function showHistory() {
-        let history = '<div class="overlay w-full">\n' +
-            '        <div class="card w-1/2 bg-gray-100 w-3/4 h-full">\n' +
-            '            <div class="max-w-7xl mx-auto">\n' +
-            '                <h2 class="card-header text-2xl font-bold mb-4">Prompt History</h2>\n' +
-            '                <div class="card-body flex flex-col h-full">\n' +
-            '                    <ul class="list-none pl-8 divide-y divide-gray-300 overflow-auto">';
+
+        let history = '<ul class="list-none mt-4 pl-8 divide-y divide-slate-400 overflow-auto">';
 
         if (window.savedStrings.length === 0) {
             history = history + '<li class="py-4 px-6 {{ $loop->last ? \'\' : \'border-b\' }}">\n' +
-                '                       <p class="text-gray-500 leading-tight">\n' + 'No history yet!' + '</p>\n' +
-                '               </li>'
+                '   <p class="text-gray-500 leading-tight">\n' + 'No history yet!' + '</p>\n' +
+                '</li>'
         } else {
             for (let i = 0; i < window.savedStrings.length; i++) {
-                history = history + '<li class="mb-4">\n' +
-                    '                       <p class="text-gray-500 leading-tight">\n' + window.savedStrings[i] + '</p>\n' +
-                    '               </li>'
+                history = history + '<li class="m-auto py-2 odd:bg-slate-50 even:bg-white">\n' +
+                    '<div class="grid grid-cols-12 pt-1 m-auto px-12">\n' +
+                    '    <div class="historyPrompt col-span-11 ">' + window.savedStrings[i] + '</div>\n' +
+                    '    <button class="copyFromHistroybtn col-span-1"> <i className="fas fa-copy"></i> </button>\n' +
+                    '</div>\n' +
+                    '</li>'
             }
         }
 
-        history = history + '     </ul>\n' +
-            '                </div>\n' +
-            '                <div class="card-footer flex content-end flex-row-reverse">\n' +
-            '                    <button class="close-btn btn btn-primary px-4 ml-2 mt-2 rounded-md self-star">\n' +
-            '                        Close\n' +
-            '                    </button>\n' +
-            '                    <button id ="clear-history" class="btn btn-primary py-2 px-4 ml-2 mt-2 rounded-md self-start">\n' +
-            '                        Clear History\n' +
-            '                    </button>\n' +
-            '                </div>' +
-            '        </div>\n' +
-            '    </div>';
+        history = history + '</ul>\n';
 
 
-        $("#overlayHistory").html(history);
+        $("#overlayContent").html(history);
+
+        $("#overlayHistory").removeClass('hidden');
+        $("#overlayHistory div i ").addClass('fas fa-copy');
+
 
         $(".overlay").click(function (event) {
             if ($(event.target).hasClass("overlay")) {
-                $(this).remove();
+                $('#overlayHistory .card-body').attr('style', '');
+                $("#overlayContent").html('');
+                $("#overlayHistory").addClass('hidden');
             }
         });
 
         $(".close-btn").click(function () {
-            $(".overlay").remove();
+            $('#overlayHistory .card-body').attr('style', '');
+            $("#overlayContent").html('');
+            $("#overlayHistory").addClass('hidden');
         });
     };
 
     $("#clear-history").click(function () {
+        $('#overlayHistory .card-body').attr('style', '');
+        $("#overlayContent").html('');
+        $("#overlayHistory").addClass('hidden');
+        $('#pre-prompt-2').text('');
+        $('#pre-prompt-1').text('');
+        $('#post-prompt-2').text('');
+        $('#post-prompt-1').text('');
         window.savedStrings = [];
+    });
+
+    $(document).on('click', '.copyFromHistroybtn', function () {
+        let prompt = $(this).parent().find('.historyPrompt').text();
+        navigator.clipboard.writeText("/imagine prompt: " + prompt);
     });
 
     $.extend(window, {
