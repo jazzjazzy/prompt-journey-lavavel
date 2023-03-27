0f7d0867a828b0f5f0a2b6652380e4a6def2e2d7
jazzjazzy <jsjazzau@gmail.com>
2023-03-28 09:58:33 +1100

    added not committed files for - added stripe cancel subscription - added stripe resume canceled subscription - design changes for payments

---
+++ database/seeders/SubscriptionsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Subscription;
+
+class SubscriptionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Subscription::create(
+            ['user_id' => 1,
+                'name' => '4',
+                'stripe_id' => 'sub_1Mpk73CohdRQHIZsFm4MWSe5',
+                'stripe_status' => 'active',
+                'stripe_price' => 'price_1ModfXCohdRQHIZsOj5KgE0s',
+                'quantity' => 1,
+                'trial_ends_at' => null,
+                'ends_at' => null,
+
+            ]
+        );
+    }
+}
