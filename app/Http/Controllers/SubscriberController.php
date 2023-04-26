67a0ed604305738c7a5b0ad5a66a41eebe55ae00
jazzjazzy <jsjazzau@gmail.com>
2023-04-26 15:36:34 +1000

    fixed payment subscription to work with one-off and reoccurring payment

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -8,6 +8,11 @@ use Laravel\Cashier\Exceptions\PaymentActionRequiredException;
 use Laravel\Cashier\Exceptions\PaymentFailureException;
 use App\Models\Plan;
 use App\Models\Project;
+use Stripe\Stripe;
+use Stripe\Price;
+use Stripe\Charge;
+use Stripe\PaymentIntent;
+use \Stripe\Exception\CardException;
 
 class SubscriberController
 {
@@ -18,7 +23,7 @@ class SubscriberController
         return view('subscription.subscribe', compact("user", "plan", "intent"));
     }
 
-    public function processSubscription(Request $request)
+    /*public function processSubscription(Request $request)
     {
         // Get the selected subscription plan from the form
         $plan = Plan::find($request->plan);
@@ -41,6 +46,66 @@ class SubscriberController
         } catch (PaymentFailureException $e) {
             // Payment failed, redirect back to the subscription page with an error message
             return back()->with('error', $e->getMessage())->withInput();
+        }
+    }*/
+
+    public function processSubscription(Request $request)
+    {
+        // Get the selected subscription plan from the form
+        $plan = Plan::find($request->plan);
+        // Get the authenticated user
+        $user = Auth::user();
+
+        $paymentMethod = $request->get('payment_method');
+        $paymentType = $request->get('payment-type');
+
+        if ($paymentType === 'one-time') {
+            try {
+                Stripe::setApiKey(env('STRIPE_SECRET'));
+
+                $stripeCharge = PaymentIntent::create([
+                    'amount' => $plan->price * 100,
+                    'currency' => 'usd',
+                    'description' => 'One-Time Payment',
+                    'payment_method' => $request->payment_method,
+                    'confirm' => true,
+                ]);
+
+                $project = new Project(['name' => 'Default', 'description' => 'My first project']);
+                $user->projects()->save($project);
+
+                // Payment created successfully, redirect to a success page
+                return view("subscription.subscribe_success", compact('user', 'plan', 'request'));
+            } catch (CardException $e) {
+                // Card error, redirect back to the subscription page with an error message
+                return back()->with('error', $e->getError()->message)->withInput();
+            }
+        } else {
+            // Handle recurring subscription
+            try {
+                // Create a new subscription for the user
+                $user->newSubscription($request->plan, $plan->stripe_id)
+                    ->create($paymentMethod, [
+                        'name' => $request->name,
+                        'email' => $user->email,
+                        'payment_method' => $request->payment_method,
+                        'address' => [
+                            'postal_code' => $request->postal_code,
+                        ],
+                    ]);
+
+                $project = new Project(['name' => 'Default', 'description' => 'My first project']);
+                $user->projects()->save($project);
+
+                // Subscription created successfully, redirect to a success page
+                return view("subscription.subscribe_success", compact('user', 'plan', 'request'));
+            } catch (PaymentActionRequiredException $e) {
+                // Payment action required, redirect to a payment page
+                return redirect($e->payment->getHostedUrl())->withInput();
+            } catch (PaymentFailureException $e) {
+                // Payment failed, redirect back to the subscription page with an error message
+                return back()->with('error', $e->getMessage())->withInput();
+            }
         }
     }
 
--- resources/views/subscription/subscribe.blade.php
+++ resources/views/subscription/subscribe.blade.php
@@ -11,6 +11,12 @@
             <div class="card">
                 <div class="card-header">
                     You will be charged ${{ number_format($plan->price, 2) }} for {{ $plan->name }} Plan
+                    <input type="hidden" name="plan-id" value="{{$plan->id}}">
+                    @if($plan->id == 2)
+                    <input type="hidden" name="payment-type" id="payment-type" value="one-time" >
+                    @else
+                    <input type="hidden" name="payment-type" id="payment-type" value="recurring">
+                    @endif
                 </div>
 
                 <div class="card-body">
@@ -19,8 +25,8 @@
                     <div class="row">
                         <div class="col-xl-4 col-lg-4">
                             <div class="form-group">
-                                <label for="">Name</label>
-                                <input type="text" name="name" id="card-holder-name" class="form-control" value=""
+                                <span for="name">Name</span>
+                                <input type="text" name="name" id="card-holder-name" value="" autocomplete="no"
                                        placeholder="Name on the card">
                             </div>
                         </div>
@@ -29,20 +35,38 @@
                     <div class="row">
                         <div class="col-xl-4 col-lg-4">
                             <div class="form-group">
-                                <label for="">Card details</label>
-                                <div id="card-element"></div>
+                                <label>
+                                    <span>Card number</span>
+                                    <div id="card-number-element" class="field"></div>
+                                </label>
+                                <label>
+                                    <span>Expiry date</span>
+                                    <div id="card-expiry-element" class="field"></div>
+                                </label>
+                                <label>
+                                    <span>CVC</span>
+                                    <div id="card-cvc-element" class="field"></div>
+                                </label>
+                                <label>
+                                    <span>Postal code</span>
+                                    <input id="postal-code" name="postal_code" class="field"
+                                           placeholder="Regular field placeholder"/>
+                                </label>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="card-footer footer-right">
                     <div class="col-xl-12 col-lg-12">
-                        <a href="{{route('subscription.pricing')}}" class="btn btn-primary text-center w-30 inline-block" id="cancel-button">
+                        <a href="{{route('subscription.pricing')}}"
+                           class="btn btn-primary text-center w-30 inline-block" id="cancel-button">
                             Cancel
                         </a>
-                        <button type="submit" class="btn btn-primary" id="card-button"
+                        <input type="hidden" name="payment_method" id="payment-method-id">
+                        <button type="submit" class="btn btn-primary" id="submit-button"
                                 data-secret="{{ $intent->client_secret }}">Purchase
                         </button>
+                        <div id="error-message" class="text-danger"></div>
 
                     </div>
                 </div>
@@ -59,57 +83,93 @@
 <script>
     const stripe = Stripe('{{ env('STRIPE_KEY') }}');
 
+    var style = {
+        base: {
+            iconColor: '#666EE8',
+            color: '#31325F',
+            fontWeight: 300,
+            fontFamily: 'Helvetica Neue',
+            fontSize: '15px',
+
+            '::placeholder': {
+                color: '#CFD7E0',
+            },
+        },
+    };
+
     const elements = stripe.elements()
-    const cardElement = elements.create('card', {
-        style: {
-            base: {
-                fontFamily: 'Source Code Pro, monospace',
-                fontSize: '16px',
-                color: '#495057',
-                '::placeholder': {
-                    color: '#aab7c4',
-                },
-            },
-            invalid: {
-                color: '#dc3545',
-                ':focus': {
-                    color: '#dc3545',
-                },
-            },
-        }
-    })
 
-    cardElement.mount('#card-element')
+    var cardNumberElement = elements.create('cardNumber', {
+        style: style,
+        placeholder: 'Custom card number placeholder',
+    });
+    cardNumberElement.mount('#card-number-element');
 
-    const form = document.getElementById('payment-form')
-    const cardBtn = document.getElementById('card-button')
-    const cardHolderName = document.getElementById('card-holder-name')
+    var cardExpiryElement = elements.create('cardExpiry', {
+        style: style,
+        placeholder: 'Custom expiry date placeholder',
+    });
+    cardExpiryElement.mount('#card-expiry-element');
 
-    form.addEventListener('submit', async (e) => {
-        e.preventDefault()
+    var cardCvcElement = elements.create('cardCvc', {
+        style: style,
+        placeholder: 'Custom CVC placeholder',
+    });
+    cardCvcElement.mount('#card-cvc-element');
 
-        cardBtn.disabled = true
-        const {setupIntent, error} = await stripe.confirmCardSetup(
-            cardBtn.dataset.secret, {
-                payment_method: {
-                    card: cardElement,
+    function displayError(errorMessage) {
+        const errorElement = document.getElementById('error-message');
+        errorElement.textContent = errorMessage;
+    }
+
+    [cardNumberElement, cardExpiryElement, cardCvcElement].forEach(element => {
+        element.on('change', event => {
+            if (event.error) {
+                displayError(event.error.message);
+            } else {
+                displayError('');
+            }
+        });
+    });
+
+    const form = document.getElementById('payment-form');
+    const submitButton = document.getElementById('submit-button');
+    const paymentMethodIdInput = document.getElementById('payment-method-id');
+
+    form.addEventListener('submit', async (event) => {
+            event.preventDefault();
+
+            let paymentTypeInput = document.getElementById('payment-type').value;
+
+                // Disable the submit button to prevent multiple submissions
+                submitButton.disabled = true;
+
+                // Create a Payment Method
+                const {paymentMethod, error} = await stripe.createPaymentMethod({
+                    type: 'card',
+                    card: cardNumberElement,
                     billing_details: {
-                        name: cardHolderName.value
-                    }
+                        name: document.getElementById('card-holder-name').value,
+                        address: {
+                            postal_code: document.getElementById('postal-code').value,
+                        },
+                    },
+                });
+
+                if (error) {
+                    // Show an error message
+                    displayError(error.message);
+                    submitButton.disabled = false;
+                } else {
+                    // Set the Payment Method ID in the hidden input field
+                    paymentMethodIdInput.value = paymentMethod.id;
+
+                    // Submit the form with the payment method ID
+                    form.submit();
                 }
             }
-        )
+    );
 
-        if (error) {
-            cardBtn.disable = false
-        } else {
-            let token = document.createElement('input')
-            token.setAttribute('type', 'hidden')
-            token.setAttribute('name', 'token')
-            token.setAttribute('value', setupIntent.payment_method)
-            form.appendChild(token)
-            form.submit();
-        }
-    })
 </script>
 @endsection
+
